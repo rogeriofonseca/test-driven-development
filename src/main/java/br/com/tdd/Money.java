17602976ebc1e1c6b7041439c6fec3d91785849b
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2016-08-14 23:43:29 -0300

    Cap 11 - A raiz de todo o Mal. Retirando subclasses desnecessarias ate o momento.

--- src/main/java/br/com/tdd/Money.java
+++ src/main/java/br/com/tdd/Money.java
@@ -18,7 +18,7 @@ public class Money {
     }
 
     Money times(int multipler){
-        return null;
+        return new Money(amount * multipler, currency);
     }
     
     public boolean equals(Object object){
@@ -27,11 +27,11 @@ public class Money {
    }
 
     static Money dollar(int amount){
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
     
     static Money franc(int amount){
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
     
     public String toString(){
